0b7cd23b4c3b15989f572e463609b1e5c7efb5ca
EGA65 <edouard@smartconcept.be>
2017-11-22 18:07:29 +0100

    aa

--- src/StartHere.java
+++ src/StartHere.java
@@ -2,7 +2,7 @@
 public class StartHere {
 	public static void main(String arg[]) {
 
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			FirstWindow fw = new FirstWindow();
 			fw.setVisible(true);
 		}
